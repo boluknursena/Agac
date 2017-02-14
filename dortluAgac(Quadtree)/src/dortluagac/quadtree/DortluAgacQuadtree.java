6b09b6e3e66bb9927dd25ed4a3efd08133148703
boluknursena <150202049@kocaeli.edu.tr>
2017-02-14 10:15:03 +0200

    Revert "Update DortluAgacQuadtree.java"
    This reverts commit db767b08083be97d952272e7e3525faeec0e0f38.

--- dortluAgac(Quadtree)/src/dortluagac/quadtree/DortluAgacQuadtree.java
+++ dortluAgac(Quadtree)/src/dortluagac/quadtree/DortluAgacQuadtree.java
@@ -3,15 +3,17 @@ package dortluagac.quadtree;
 class Dugum {
 
     int x, y;
-    Dugum gb, gd, kd, kb;
+    Dugum dogu, batı, kuzey, guney;
+    String bilgi;
 
-    Dugum(int x, int y) {
+    Dugum(String bilgi, int x, int y) {
+        this.bilgi = bilgi;
         this.x = x;
         this.y = y;
     }
 
     public String toString() {
-        return " x: " + x + " y: " + y;
+        return "Bilgi: " + bilgi + " x: " + x + " y: " + y;
     }
 }
 
@@ -20,47 +22,13 @@ public class DortluAgacQuadtree {
     Dugum kok;
 
     public void dugumEkle(String bilgi, int x, int y) {
-        
-        
-        Dugum yeniDugum = new Dugum(x,y);
+        Dugum yeniDugum = new Dugum(bilgi, x, y);
         if (kok == null) {
             kok = yeniDugum;
         } else {
             Dugum odaklananDugum = kok;
-            Dugum aile;
-            while (true) {
-                aile = odaklananDugum;
-                if (x < odaklananDugum.x && y < odaklananDugum.y) {
-                    odaklananDugum = odaklananDugum.gb;
-                    if (odaklananDugum == null) {
-                        aile.gb = yeniDugum;
-                        return;
-                    }
-                }
-                else if (x >= odaklananDugum.x && y < odaklananDugum.y) {
-                    odaklananDugum = odaklananDugum.gd;
-                    if (odaklananDugum == null) {
-                        aile.gd = yeniDugum;
-                        return;
-                    }
-                }
-                else if (x >= odaklananDugum.x && y >= odaklananDugum.y) {
-                    odaklananDugum = odaklananDugum.kd;
-                    if (odaklananDugum == null) {
-                        aile.kd = yeniDugum;
-                        return;
-                    }
-                }else {
-                    odaklananDugum = odaklananDugum.kb;
-                    if (odaklananDugum == null) {
-                        aile.kb = yeniDugum;
-                        return;
-                    }
-                }
-            }
+
         }
-        
-        
     }
 
     public static void main(String[] args) {
